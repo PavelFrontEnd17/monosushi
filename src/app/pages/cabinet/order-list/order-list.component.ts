0e8b0d462849a6f33064e5cdb0d7bce75d02268a
PavelFrontEnd17 <sergeevpavel923@gmail.com>
2023-08-26 15:33:32 +0300

    order-list component of cabinet module added

---
+++ src/app/pages/cabinet/order-list/order-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.scss']
+})
+export class OrderListComponent {
+
+
+
+  constructor(
+
+  ){}
+
+
+  ngOnInit(): void {
+    this.getUser()
+  }
+  public curentUser = JSON.parse(localStorage.getItem('currentUser') as string)
+  public orders = this.curentUser.orders
+  getUser(){
+    this.curentUser = JSON.parse(localStorage.getItem('currentUser') as string)
+    this.orders = this.curentUser.orders
+
+  }
+
+  
+  
+}
