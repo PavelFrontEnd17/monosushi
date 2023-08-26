057bc02264e8ad6a099a9d9931302b8250c967cc
PavelFrontEnd17 <sergeevpavel923@gmail.com>
2023-08-26 15:23:21 +0300

    app files added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,71 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import { DiscountsComponent } from './pages/about-us/discounts/discounts.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { DeliveryAndPaymentComponent } from './pages/about-us/delivery-and-payment/delivery-and-payment.component';
+import { DisountsInfoComponent } from './pages/about-us/disounts-info/disounts-info.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCategoriesComponent } from './admin/admin-categories/admin-categories.component';
+import { AdminDiscountsComponent } from './admin/admin-discounts/admin-discounts.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { ProductInfoComponent } from './pages/product/product-info/product-info.component';
+import { ProductsService } from './shared/services/products/products.service';
+import { DiscountsService } from './shared/services/discounts/discounts.service';
+import { AuthGuard} from './shared/guards/auth/auth.guard';
+import { CabinetComponent } from './pages/cabinet/cabinet.component';
+import { MainComponent } from './pages/cabinet/main/main.component';
+import { OrderListComponent } from './pages/cabinet/order-list/order-list.component';
+import { ChangePassComponent } from './pages/cabinet/change-pass/change-pass.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+import { getRedirectResult } from '@firebase/auth';
+import { AdminAuthComponent } from './pages/auth/admin-auth/admin-auth.component';
+import {CabinetGuard} from "./shared/guards/cab/cabinet.guard";
+import { PhoningComponent } from './pages/phoning/phoning.component';
 
-const routes: Routes = [];
+
+function redirect(){
+
+}
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'check-out', component: CheckOutComponent},
+  {
+    path: 'product/:category',
+    component: ProductComponent,
+    loadChildren: () => import('./pages/product/product.module').then(m=> m.ProductModule)
+  },
+  {path: 'product/:category/:id', component: ProductInfoComponent, resolve:{
+    productInfo: ProductsService
+  }},
+  {path: 'discounts/:id', component: DisountsInfoComponent, resolve:{
+    discountInfo: DiscountsService
+  }},
+  {
+    path: 'cabinet',
+    canActivate: [CabinetGuard],
+    loadChildren: () => import('./pages/cabinet/cabinet.module').then(m => m.CabinetModule)
+  },
+  {path: 'admin-auth', component: AdminAuthComponent},
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'about-us',
+    loadChildren: () => import('./pages/about-us/about-us.module').then(m => m.AboutUsModule)
+  },
+
+  {path: 'phoning', component: PhoningComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,  {
+      preloadingStrategy:  PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,102 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IMaskModule } from 'angular-imask';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+
+
+import { environment } from '../environments/environment';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AdminComponent } from './admin/admin.component';
+import { AdminDiscountsComponent } from './admin/admin-discounts/admin-discounts.component';
+import { AdminCategoriesComponent } from './admin/admin-categories/admin-categories.component';
+import { CategoriesService } from './shared/services/caategories/categories.service';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DisountsInfoComponent } from './pages/about-us/disounts-info/disounts-info.component';
+import { ProductComponent } from './pages/product/product.component';
+import { DiscountsComponent } from './pages/about-us/discounts/discounts.component';
+import { DeliveryAndPaymentComponent } from './pages/about-us/delivery-and-payment/delivery-and-payment.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { ProductInfoComponent } from './pages/product/product-info/product-info.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { CabinetComponent } from './pages/cabinet/cabinet.component';
+import { MainComponent } from './pages/cabinet/main/main.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+import { ChangePassComponent } from './pages/cabinet/change-pass/change-pass.component';
+import { OrderListComponent } from './pages/cabinet/order-list/order-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+import { RegComponent } from './pages/auth/reg/reg.component';
+import { SharedModule } from './shared/shared.module';
+import { AdminAuthComponent } from './pages/auth/admin-auth/admin-auth.component';
+import { AdminModule } from './admin/admin.module';
+import { ProductModule } from './pages/product/product.module';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import { PhoningComponent } from './pages/phoning/phoning.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDiscountsComponent,
+    AdminCategoriesComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DisountsInfoComponent,
+    DiscountsComponent,
+    DeliveryAndPaymentComponent,
+    AboutUsComponent,
+    ProductsComponent,
+    // ProductComponent,
+    // ProductInfoComponent,
+    AuthComponent,
+    MainComponent,
+    CheckOutComponent,
+    ChangePassComponent,
+    OrderListComponent,
+    RegComponent,
+    AdminAuthComponent,
+    PhoningComponent
+
   ],
   imports: [
+    
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IMaskModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage()),
+    provideFirestore(()=>getFirestore()),
+    provideAuth(()=>getAuth()),
+    BrowserAnimationsModule,
+    SharedModule,
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
