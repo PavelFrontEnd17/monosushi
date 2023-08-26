2f7690df27cb49a713b6445c07e7bc98d858771c
PavelFrontEnd17 <sergeevpavel923@gmail.com>
2023-08-26 15:39:27 +0300

    environment files added

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'monosushi-dbac9',
+    appId: '1:74560866768:web:40df389f08d67c0cfc91d5',
+    storageBucket: 'monosushi-dbac9.appspot.com',
+    apiKey: 'AIzaSyBm8iDVM6bv0OYxIkk5MgWwtcAMblS2VEY',
+    authDomain: 'monosushi-dbac9.firebaseapp.com',
+    messagingSenderId: '74560866768',
+  },
+  production: false,
+  BACKEND_URL: `http://localhost:3000`
+};
