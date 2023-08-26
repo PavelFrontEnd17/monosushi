f29c75ea1870d13d84c188d169706a5d9050d757
PavelFrontEnd17 <sergeevpavel923@gmail.com>
2023-08-26 15:37:52 +0300

    auth service files added

---
+++ src/app/shared/services/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ILogin, IUpdate } from '../../interfaces/auth/auth.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public isUserLogin$ = new Subject<boolean>();
+  private url = environment.BACKEND_URL;
+  private api = {
+    auth: `${this.url}/auth`,
+  }
+  constructor(
+    private http: HttpClient
+  ) { }
+  login(credential: ILogin):Observable<any>{
+    console.log(credential)
+    return this.http.get(`${this.api.auth}?email=${credential.email}&&pass=${credential.pass}`)
+  }
+
+  change(data:IUpdate, id: number){
+    console.log(data)
+    return this.http.patch<IUpdate>(`${this.api.auth}/${id}`,data)
+  }
+}
